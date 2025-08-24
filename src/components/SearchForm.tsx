4ea5e0ac910c5cc35cc4fd18726cc4424ca5ec27
ammaritto <ammar@allihoop.se>
2025-08-24 19:06:27 +0200

    guests in searchform

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -272,10 +272,10 @@ const SearchForm: React.FC<SearchFormProps> = ({
                 Guests
               </label>
               <div className="relative">
-                <Users className="absolute left-3 top-1/2 transform -translate-y-1/2 h-5 w-5 text-gray-400" />
+                <Users className="absolute left-3 top-1/2 transform -translate-y-1/2 h-5 w-5 text-gray-400 z-10" />
                 <select 
                   id="guests" 
-                  className="w-full pl-10 pr-8 py-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent appearance-none" 
+                  className="w-full h-12 pl-10 pr-4 py-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-blue-500 focus:outline-none text-gray-900 bg-white appearance-none" 
                   value={searchParams.guests} 
                   onChange={e => setSearchParams(prev => ({
                     ...prev,
