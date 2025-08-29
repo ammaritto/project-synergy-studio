5f68f0f4bdcd5ea1f89222cf6c72773f55da719e
ammaritto <ammar@allihoop.se>
2025-08-29 12:36:23 +0200

    wwww

--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -48,8 +48,8 @@ const SearchResults: React.FC<SearchResultsProps> = ({
       [key: number]: string;
     } = {
       38: 'https://cdn.prod.website-files.com/606d62996f9e70103c982ffe/680a675aca567cd974c649a9_ANG-Studio-ThumbnailComp-min.png',
-      11: 'https://cdn.prod.website-files.com/606d62996f9e70103c982ffe/67a0e42f26139e1447842db9_679b776e8739d51e5d09b6e0_BROStudioThumbnail.png',
-      10: 'https://cdn.prod.website-files.com/606d62996f9e70103c982ffe/67a0e42f38f10cd500444414_679b77b10e3fbe69fb60cc4c_BROStudioPlusThumbnail.png'
+      10: 'https://cdn.prod.website-files.com/606d62996f9e70103c982ffe/67a0e42f26139e1447842db9_679b776e8739d51e5d09b6e0_BROStudioThumbnail.png',
+      11: 'https://cdn.prod.website-files.com/606d62996f9e70103c982ffe/67a0e42f38f10cd500444414_679b77b10e3fbe69fb60cc4c_BROStudioPlusThumbnail.png'
     };
     return imageMap[inventoryTypeId] || 'https://via.placeholder.com/400x240/e5e7eb/9ca3af?text=Photo+Coming+Soon';
   };
@@ -59,7 +59,7 @@ const SearchResults: React.FC<SearchResultsProps> = ({
     } = {
       10: {
         sqm: 15,
-        bathroom: 'Shared',
+        bathroom: 'Private',
         kitchen: 'Shared',
         rooms: 1,
         readMoreUrl: 'https://www.allihoopliving.com/studios/bromma-studio'
@@ -74,8 +74,8 @@ const SearchResults: React.FC<SearchResultsProps> = ({
     };
     return detailsMap[inventoryTypeId] || {
       sqm: 15,
-      bathroom: 'Shared',
-      kitchen: 'Shared',
+      bathroom: 'Private',
+      kitchen: '`Private`',
       rooms: 1,
       readMoreUrl: '#'
     };
